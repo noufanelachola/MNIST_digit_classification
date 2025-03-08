e9bc65721f5373917331977b9ddd7b5a8b79706c
noufanelachola <noufansadiqelachola@gmail.com>
2025-03-08 17:36:48 +0530

    Loaded and preprocessed

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+
+# Step 1: Load & Preprocess Data (Flatten & Normalize)
+# Step 2: Train Different ML Models
+    # Logistic Regression (Baseline)
+    # SVM (Support Vector Machine) (Stronger Model)
+    # Random Forest (Tree-Based Approach)   
+    # KNN (K-Nearest Neighbors) (Alternative)
+# Step 3: Compare Model Performance
+# Step 4: Hyperparameter Tuning for Best Accuracy
+# Step 5: Final Model Selection & Testing on Unseen Data
+
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 (X_train,y_train),(X_test,y_test) = keras.datasets.mnist.load_data()
 
-plt.imshow(X_train[1], cmap="gray")  
-plt.title(f"Label: {y_train[1]}")  
-plt.show()
+# Flatten the images
+X_train = X_train.reshape(60000,784)
+X_test = X_test.reshape(10000,784)
+
+# Normalise the data
+X_train = X_train/255
+X_test = X_test/255
+
+# Split the training data
+X_train,X_val,y_train,y_val = train_test_split(X_train,y_train,test_size=0.2,random_state=42)
+
 
 print(f"Train shape : {X_train.shape}")
 print(f"Test shape : {X_test.shape}")
+print(type(X_train))
+
+
+# Show the image
+plt.imshow(X_train[0].reshape(28,28), cmap="gray")
+plt.title(f"{y_train[0]}")
+plt.show()
